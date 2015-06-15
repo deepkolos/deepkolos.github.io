d8f75d1e0061256f58cc15b3868300184f62ab60
deepkolos <k1206521365k@live.cn>
2015-06-15 12:22:14 +0800

    导航栏finish

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var $debug = document.getElementById("debug");
-$("#aboutme").click(function (){show_detail();});
-$('#mainbox').click(function (){hide_detail();});
+ 	$("#aboutme").click(function (){show_detail();});
+	$("#mainbox").click(function (){hide_detail();});
+	b_beforeclicked_id = -1 ;
+var hide_detail_t
+	
 function show_detail(){
 	$("#detail").css("margin","0 0 0 -10px");
 	$('#mainbox').css('z-index','80');
@@ -11,11 +14,40 @@ function hide_detail(){
 		$('#mainbox').css('z-index','-1');	
 		clearTimeout(hide_detail_t);
 	};
-/*导航栏*/
-$("#nav span").click(function (){
-	
+//导航栏
+$("#b_nav0").click(function (){
+	var b_nowclicked_id = 0;
+	switch_pages(b_nowclicked_id,b_beforeclicked_id);
 });
-
+$("#b_nav1").click(function (){
+	var b_nowclicked_id = 1;
+	switch_pages(b_nowclicked_id,b_beforeclicked_id);
+});
+$("#b_nav2").click(function (){
+	var b_nowclicked_id = 2;
+	switch_pages(b_nowclicked_id,b_beforeclicked_id);
+});
+$("#b_nav3").click(function (){
+	var b_nowclicked_id = 3;
+	switch_pages(b_nowclicked_id,b_beforeclicked_id);
+});
+function switch_pages(n,b){
+	if (n != b) {
+		var b_nowclicked = $("#nav"+n);
+		var b_beforeclicked =$("#nav"+b);
+	b_nowclicked.css({
+		"background" : "red" ,
+		"margin-left" : "-350px" ,
+		"opacity" : "1"
+	});
+		b_beforeclicked.css({
+			"background" : "red" ,
+			"margin-left" : "0" ,
+			"opacity" : "0"
+		});	
+		b_beforeclicked_id = n;
+	}
+};
 /*
 var $column = $("#column");
 	$mainbox = $("#mainbox");
