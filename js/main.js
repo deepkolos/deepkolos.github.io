fe68f4340e99d7902713371b3b2e54d6e4c2b953
DeepKolos <k1206521365k@live.cn>
2015-08-02 11:36:29 +0800

    添加页面跳转动画

--- js/main.js
+++ js/main.js
@@ -1,165 +1,37 @@
-var $debug = document.getElementById("debug");
- 	$("#aboutme").click(function (){show_detail();});
-	$("#mainbox").click(function (){hide_detail();});
-	b_beforeclicked_id = -1 ;
-var hide_detail_t
+var url_g  //转存一下url
 
-
+//进入动画
+function wellCome(){
+	$("#column").css({"margin-top":"0px" , "opacity":"1"});
+	$("#mainBox").css({ "opacity":"1","margin-top":"50px"});
 	
-/*
-function show_detail(){
-	$("#detail").css("margin","0 0 0 -10px");
-	$('#mainbox').css('z-index','80');
-	hide_detail_t=setTimeout("hide_detail()",8000);
 }
-function hide_detail(){
-		$('#detail').css('margin','-150px 0 0 -10px');
-		$('#mainbox').css('z-index','-1');	
-		clearTimeout(hide_detail_t);
-	};
-	
-*/
-//加载xml
-var loadXML = function(xmlFile){  
-    xmlDoc=null;  
-     if (window.ActiveXObject){  
-        xmlDoc = new ActiveXObject('Msxml2.DOMDocument');  
-        xmlDoc.async=false;  
-        xmlDoc.load(xmlFile);  
-    }  
-    else if (document.implementation && document.implementation.createDocument){  
-        var xmlhttp = new window.XMLHttpRequest();  
-        xmlhttp.open("GET",xmlFile,false);  
-        xmlhttp.send(null);  
-        var xmlDoc = xmlhttp.responseXML.documentElement;   
-    }  
-    else {xmlDoc=null;}  
-    return xmlDoc;  
-}  
 
-var xml=loadXML("articles/data.xml");
-//加载首页10篇博文
-function loadLatestArticle(){
-	for (var i = 0; i < 10; i++) {
-		var article = xml.getElementsByTagName("article")[i] ;
-		var title = article.getElementsByTagName("title")[0].childNodes[0].nodeValue ;
-		var summary = article.getElementsByTagName("summary")[0].childNodes[0].nodeValue ;
-		var text = article.getElementsByTagName("text")[0].childNodes[0].nodeValue ;
-		var home = document.getElementById("nav0");
-		var category = article.getAttribute("category");
-		var year = article.getAttribute("year");
-		var month = article.getAttribute("month");
-		var day = article.getAttribute("day");
-		//创建文章卡片 method 1
-		/*
-		var home = document.getElementById("nav0")
-		var card = document.createElement("div");
-		var card_h2 = document.createElement("h2");
-		var card_span = document.createElement("span");
-		var card_div = document.createElement("div");
-		var attr = document.createAttribute("class");
-		var attr.nodeValue="article";
-		home.appendChild(card);
-		
-		*/
-		
-		//创建文章卡片 method 2
-		var card = "<div class="+"article"+" id="+"article"+i+" onclick="+"showcard("+i+")"+">";
-		card = card +"<h1>"+title+"</h1>";
-		card = card +"<div class="+"article_inf"+">日期："+year+"年"+month+"月"+day+"日 分类："+category+"</div>"
-		card = card +"<p class="+"summary"+">"+summary+"</p>";
-		card = card +"<span class="+"text"+">"+text+"</span><div class="+"fengexian"+"></div></div>"
-		var cacheHome = home.innerHTML
-		home.innerHTML=cacheHome+card;
-	}
+//离开动画
+function goodBye(){
+	$("#column").css({"margin-top":"0px" , "opacity":"0"});
+	$("#mainBox").css({"margin-top":"10%" , "opacity":"0"});
 }
-loadLatestArticle();
-
-//导航栏
-$("#b_nav0").click(function (){
-	var b_nowclicked_id = 0;
-	switch_pages(b_nowclicked_id,b_beforeclicked_id);
-});
-$("#b_nav1").click(function (){
-	var b_nowclicked_id = 1;
-	switch_pages(b_nowclicked_id,b_beforeclicked_id);
-});
-$("#b_nav2").click(function (){
-	var b_nowclicked_id = 2;
-	switch_pages(b_nowclicked_id,b_beforeclicked_id);
-});
-$("#b_nav3").click(function (){
-	var b_nowclicked_id = 3;
-	switch_pages(b_nowclicked_id,b_beforeclicked_id);
-});
-function switch_pages(n,b){
-	if (n != b) {
-		$("#nav"+n).css({
-			"background" : "white" ,
-			"margin-top" : "0" ,
-			"opacity" : "1" ,
-			"z-index" : "10"
-		});
-		$("#nav"+b).css({
-			"background" : "white" ,
-			"margin-top" : "0" ,
-			"opacity" : "1" ,
-			"z-index" : "-1"
-		});	
-		$("#b_nav"+n).css({
-			"font-weight":"bold",
-			"opacity" : "1" 
-		});
-		$("#b_nav"+b).css({
-			"font-weight":"normal",
-			"opacity" : "1"
-		});
-		b_beforeclicked_id = n;
-		
-	}
-};
-
-//展开卡片
-function showcard(n){
-	var article = document.getElementById("article"+n);
-	var title = article.getElementsByTagName("h1")[0];
-	title.style.color="red"
-	article.style.height= "100%"
+//延迟执行
+function delayRun(func,time) {
+	var thenRun = setTimeout("window.location.assign(url_g)",time)
 }
-//alert($(".article").length+"|"+$(".article")[0])
 
+//设置超链接延迟
+function openURL(url){
+	goodBye();
+	url_g = url ;
+	delayRun("window.location.assign(url_g)",400)
+}
 
+//前进后退 
+//$("#back").click(function (){goodBye();delayRun("window.history.back()",400)});
+//$("#forward").click(function (){goodBye();delayRun("window.history.froward()",400)});
 
-/*
-var $column = $("#column");
-	$mainbox = $("#mainbox");
-	DocumentEvent = $("#aboutme_text");
-	$debug = document.getElementById("debug");
-	
-/* 自适应窗体 start*
-function updatevalue(){		//更新窗体数据
-	mainbox_width = parseInt($mainbox.css("width"));
-	main_height = parseInt($mainbox.css("height"));
-	column_width = parseInt($column.css("width"));
-	column_height = parseInt($column.css("height"));
-};
-function resize(){		//调整
-	updatevalue();
-	//$column.css("width")=mainbox_width
-	
-	if (mainbox_width > 620) {
-		$debug.innerHTML=mainbox_width+"<br>"
-						+main_height+"<br>"
-						+column_width+"<br>"
-						+column_height+"<br>";
-						
-						
-		
-	}else {
-		
-	}
+//导航栏按钮
+$("#b_nav0").click(function (){openURL();});
+$("#b_nav2").click(function (){openURL();});
+$("#b_nav3").click(function (){openURL();});
 
-	
-};
-/* 自适应窗体 end /
-*/
+//dir 特别对待
+$("#b_nav1").click(function (){openURL();});
